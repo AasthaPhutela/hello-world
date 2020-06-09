6773f57cbd79137c52b3ba5f7bc3623aa50ef5f7
Aastha <aphutela_be17@thapar.edu>
2020-06-09 10:17:37 +0530

    commit

--- main.lua
+++ main.lua
@@ -1,13 +1,5 @@
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
 
-
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-
-
-=======
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -15,34 +7,17 @@ WINDOW_HEIGHT = 720
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-
-
-        resizable = true,
-
         resizable = false,
         vsync = true
     })
 end
 
 
-
 function love.draw()
     love.graphics.printf(
-        'Hello Pong!',          
-        0,                      
-        WINDOW_HEIGHT / 2 - 6,  
-        WINDOW_WIDTH,           
-        'center')               
-
-end 
+        'Hello Pong!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,  
+        'center')             
 end
-========
-end
-
-@@ -44,4 +44,4 @@ function love.draw()
-        WINDOW_HEIGHT / 2 - 6,  
-        WINDOW_WIDTH,           
-        'center')               
-end 
-end
-
